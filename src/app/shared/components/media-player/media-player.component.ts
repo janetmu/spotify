68e9e5d6a4895a9d578ac3b5dcbd85bd7d841c8d
Janet Marulanda <janet_marulanda@yahoo.com>
2023-10-07 11:57:41 -0500

    generación de servicio, inyección de servicio a componente y modularización de funciones

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '../../../core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
   mockCover: TrackModel = {
     cover: "https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc",
     album: "Giolì & Assia",
@@ -14,11 +16,23 @@ export class MediaPlayerComponent implements OnInit {
     url: "http://localhost/track.mp3",
     _id: 1
   }
+
+  listObservers$: Array<Subscription> = []
   
-  constructor () { }
+  constructor (private multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
+    const observer1$: Subscription = this.multimediaService.callBack.subscribe(
+      (response: TrackModel) => {
+        console.log('Recibiendo canción...', response);
+      } 
+    ) 
 
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    console.log('BOOM');
   }
 
 }
