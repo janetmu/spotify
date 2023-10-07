8b8a1120cb6ae43255914c57ec34d0a8eb9a5918
Janet Marulanda <janet_marulanda@yahoo.com>
2023-10-06 20:21:28 -0500

    creación formulario reactivo para login y configuración de validaciones

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators }  from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  
+  formLogin: FormGroup = new FormGroup({});
+  
+  constructor() { }
 
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', 
+      [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormGroup('',                                                                             
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),                                                                                 
+      ]),                                             
+    });
+  } 
+  
+  sendLogin(): void {
+    const body = this.formLogin.value
+    console.log(body);
+  }
 }
